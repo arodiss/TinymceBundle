86fed259672dfe4f04bc1a0488750ef5eb9263b8
Gleb Sidora <zetaro@bigmir.net>
2012-12-28 12:57:59 +0200

    added check to nor reinitialize initialized editors

--- Resources/public/js/init.jquery.js
+++ Resources/public/js/init.jquery.js
@@ -27,47 +27,51 @@ function initTinyMCE(options) {
                 var $textarea = $(this),
                     theme = $textarea.data('theme') || 'simple';
 
-                // Get selected theme options
-                var themeOptions = (typeof options.theme[theme] != 'undefined')
-                    ? options.theme[theme]
-                    : options.theme['simple'];
+                if('undefined' == typeof($textarea.attr('aria-decorated'))) {
 
-                themeOptions.script_url = options.jquery_script_url;
+                    // Get selected theme options
+                    var themeOptions = (typeof options.theme[theme] != 'undefined')
+                        ? options.theme[theme]
+                        : options.theme['simple'];
 
-                // workaround for an incompatibility with html5-validation (see: http://git.io/CMKJTw)
-                if ($textarea.is('[required]')) {
-                    themeOptions.oninit = function (editor) {
-                        editor.onChange.add(function (ed) {
-                            ed.save();
-                        });
-                    };
-                }
-                themeOptions.setup = function(ed) {
-                    // Add custom buttons to current editor
-                    $.each(options.tinymce_buttons || {}, function(id, opts) {
-                        opts = $.extend({}, opts, {
-                            onclick: function() {
-                                var callback = window['tinymce_button_' + id];
-                                if (typeof callback == 'function') {
-                                    callback(ed);
-                                } else {
-                                    alert('You have to create callback function: "tinymce_button_' + id + '"');
+                    themeOptions.script_url = options.jquery_script_url;
+
+                    // workaround for an incompatibility with html5-validation (see: http://git.io/CMKJTw)
+                    if ($textarea.is('[required]')) {
+                        themeOptions.oninit = function (editor) {
+                            editor.onChange.add(function (ed) {
+                                ed.save();
+                            });
+                        };
+                    }
+                    themeOptions.setup = function(ed) {
+                        // Add custom buttons to current editor
+                        $.each(options.tinymce_buttons || {}, function(id, opts) {
+                            opts = $.extend({}, opts, {
+                                onclick: function() {
+                                    var callback = window['tinymce_button_' + id];
+                                    if (typeof callback == 'function') {
+                                        callback(ed);
+                                    } else {
+                                        alert('You have to create callback function: "tinymce_button_' + id + '"');
+                                    }
                                 }
+                            });
+                            ed.addButton(id, opts);
+                        });
+
+                        // Load external plugins
+                        $.each(options.external_plugins || {}, function(id, opts) {
+                            var url = opts.url || null;
+                            if (url) {
+                                tinymce.PluginManager.load(id, url);
                             }
                         });
-                        ed.addButton(id, opts);
-                    });
+                    };
 
-                    // Load external plugins
-                    $.each(options.external_plugins || {}, function(id, opts) {
-                        var url = opts.url || null;
-                        if (url) {
-                            tinymce.PluginManager.load(id, url);
-                        }
-                    });
-                };
-
-                $textarea.tinymce(themeOptions);
+                    $textarea.tinymce(themeOptions);
+                    $textarea.attr('aria-decorated', true);
+                }
             });
         });
     }(jQuery));
