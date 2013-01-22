d991d3133d86518695022652e20c6d6d8f35b2a7
Gleb Sidora <zetaro@bigmir.net>
2013-01-22 15:29:16 +0200

    Commit added "tinyMCE is loaded" check to fight concurrency

--- Resources/public/js/init.jquery.js
+++ Resources/public/js/init.jquery.js
@@ -14,65 +14,71 @@ function initTinyMCE(options) {
             window.defaultTinyMCEOptions = options;
         }
 
-        $(function () {
-            var $tinymceTargets;
+        while(true) {
+            if(typeof(window.tinyMceIsInit) == 'undefined') {continue}
 
-            if(options.textarea_class){
-                $tinymceTargets = $('textarea' + options.textarea_class);
-            } else {
-                $tinymceTargets = $('textarea');
-            }
+            $(function () {
+                var $tinymceTargets;
 
-            $tinymceTargets.each(function () {
-                var $textarea = $(this),
-                    theme = $textarea.data('theme') || 'simple';
+                if(options.textarea_class){
+                    $tinymceTargets = $('textarea' + options.textarea_class);
+                } else {
+                    $tinymceTargets = $('textarea');
+                }
 
-                if('undefined' == typeof($textarea.attr('aria-decorated')) && $textarea.is(':visible')) {
+                $tinymceTargets.each(function () {
+                    var $textarea = $(this),
+                        theme = $textarea.data('theme') || 'simple';
 
-                    // Get selected theme options
-                    var themeOptions = (typeof options.theme[theme] != 'undefined')
-                        ? options.theme[theme]
-                        : options.theme['simple'];
+                    if('undefined' == typeof($textarea.attr('aria-decorated')) && $textarea.is(':visible')) {
 
-                    themeOptions.script_url = options.jquery_script_url;
+                        // Get selected theme options
+                        var themeOptions = (typeof options.theme[theme] != 'undefined')
+                            ? options.theme[theme]
+                            : options.theme['simple'];
 
-                    // workaround for an incompatibility with html5-validation (see: http://git.io/CMKJTw)
-                    if ($textarea.is('[required]')) {
-                        themeOptions.oninit = function (editor) {
-                            editor.onChange.add(function (ed) {
-                                ed.save();
-                            });
-                        };
-                    }
-                    themeOptions.setup = function(ed) {
-                        // Add custom buttons to current editor
-                        $.each(options.tinymce_buttons || {}, function(id, opts) {
-                            opts = $.extend({}, opts, {
-                                onclick: function() {
-                                    var callback = window['tinymce_button_' + id];
-                                    if (typeof callback == 'function') {
-                                        callback(ed);
-                                    } else {
-                                        alert('You have to create callback function: "tinymce_button_' + id + '"');
+                        themeOptions.script_url = options.jquery_script_url;
+
+                        // workaround for an incompatibility with html5-validation (see: http://git.io/CMKJTw)
+                        if ($textarea.is('[required]')) {
+                            themeOptions.oninit = function (editor) {
+                                editor.onChange.add(function (ed) {
+                                    ed.save();
+                                });
+                            };
+                        }
+                        themeOptions.setup = function(ed) {
+                            // Add custom buttons to current editor
+                            $.each(options.tinymce_buttons || {}, function(id, opts) {
+                                opts = $.extend({}, opts, {
+                                    onclick: function() {
+                                        var callback = window['tinymce_button_' + id];
+                                        if (typeof callback == 'function') {
+                                            callback(ed);
+                                        } else {
+                                            alert('You have to create callback function: "tinymce_button_' + id + '"');
+                                        }
                                     }
+                                });
+                                ed.addButton(id, opts);
+                            });
+
+                            // Load external plugins
+                            $.each(options.external_plugins || {}, function(id, opts) {
+                                var url = opts.url || null;
+                                if (url) {
+                                    tinymce.PluginManager.load(id, url);
                                 }
                             });
-                            ed.addButton(id, opts);
-                        });
+                        };
 
-                        // Load external plugins
-                        $.each(options.external_plugins || {}, function(id, opts) {
-                            var url = opts.url || null;
-                            if (url) {
-                                tinymce.PluginManager.load(id, url);
-                            }
-                        });
-                    };
-
-                    $textarea.tinymce(themeOptions);
-                    $textarea.attr('aria-decorated', true);
-                }
+                        $textarea.tinymce(themeOptions);
+                        $textarea.attr('aria-decorated', true);
+                    }
+                });
             });
-        });
-    }(jQuery));
+
+            break; }
+
+        }(jQuery));
 }
